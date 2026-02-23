7203221708abd3fc80b674b2b60499da18e1ba52
Nantapong <139741180+detafastnoon@users.noreply.github.com>
2026-02-23 15:00:35 +0700

    Add files via upload

---
+++ 68016448_L8/myproject/myapp/views.py
@@ -0,0 +1,14 @@
+# myapp/views.py
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html') 
+
+def about(request):
+    return render(request, 'about.html')
+
+def form(request):
+    return render(request, 'form.html')
+
+def contact(request):
+    return render(request, 'contact.html')
